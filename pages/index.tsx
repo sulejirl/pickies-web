319fb5134117484b9dd1e4bc40372f2943cc62dd
Ahmet Sayarlıoğlu <ahmet.sayarlioglu@gmail.com>
2023-01-12 21:54:20 +0100

    privacy policy page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,161 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
+import Image from "next/image";
 
 export default function Home({
-  allPostsData
+	allPostsData,
 }: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+	allPostsData: {
+		date: string;
+		title: string;
+		id: string;
+	}[];
 }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section>
+				<div
+					style={{
+						width: "100%",
+						height: "1000px",
+						position: "relative",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<Image
+						src={"/images/FirstRowImage.svg"}
+						alt="Line1"
+						fill
+						style={{
+							background: "#53beb3ff",
+							objectFit: "cover",
+						}}
+					/>
+					<div
+						style={{
+							position: "absolute",
+							textAlign: "center",
+							color: "white",
+							fontSize: "3.7rem",
+							maxWidth: "470px",
+							lineHeight: "70px",
+						}}
+					>
+						Pickies ile siparişini ver,{" "}
+						<i>sıra beklemeden teslim al!</i>
+					</div>
+				</div>
+				<div
+					style={{
+						width: "100%",
+						height: "300px",
+						background: " #fffdedff",
+						position: "relative",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<div
+						style={{
+							position: "absolute",
+							maxWidth: "650px",
+							fontFamily: "Work Sans",
+							fontSize: "19px",
+							letterSpacing: "3px",
+							lineHeight: "30px",
+							textAlign: "center",
+						}}
+					>
+						SADECE TEK BİR TIKLA BULUNDUĞUN BÖLGEDEKİ CAFE VE
+						RESTORANLARIN MENÜLERİNE ULAŞ, SİPARİŞİNİ VER VE
+						<b>
+							{" "}
+							PICIKIES TESLİM NOKTASINDAN SANİYELER İÇİNDE
+							SİPARİŞİNİ AL
+						</b>
+					</div>
+				</div>
+				<div
+					style={{
+						width: "100%",
+						height: "600px",
+						background: " #f7b76aff",
+						position: "relative",
+						display: "grid",
+						gridTemplateColumns: " 1fr 1fr",
+						gridTemplateRows: "1fr",
+						gap: "0px 0px",
+						gridTemplateAreas: " 'image text'",
+						justifyItems: "center",
+						alignItems: "center",
+					}}
+				>
+					<div
+						style={{
+							width: "400px",
+							height: "400px",
+							position: "absolute",
+							gridArea: "image",
+						}}
+					>
+						<Image
+							src={"/images/ThirdRowImage.svg"}
+							alt="Line1"
+							fill
+							style={{
+								objectFit: "contain",
+							}}
+						/>
+						<div
+							style={{
+								position: "absolute",
+								top: "50%",
+								left: "50%",
+								transform:
+									"translate(-50%, -50%) rotate(-7deg)",
+								textAlign: "center",
+								color: "#f7b76aff",
+								fontSize: "3rem",
+								fontWeight: "bold",
+								maxWidth: "470px",
+								lineHeight: "50px",
+								letterSpacing: "-3px",
+								fontFamily: "Clearface",
+							}}
+						>
+							Pickies Mobil Uygulama Nedir?
+						</div>
+					</div>
+					<div style={{ gridArea: "text" }}>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing
+						elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+						dolore magna aliquam erat volutpat. Ut wisi enim ad
+						minim veniam, quis nostrud exerci tation ullamcorper
+						suscipit lobortis nisl ut aliquip ex ea commodo
+						consequat.
+					</div>
+				</div>
+			</section>
+		</Layout>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+};
