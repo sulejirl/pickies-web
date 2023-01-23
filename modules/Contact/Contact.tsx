3085cc94df022dff081125610bc831f014aba97a
Ahmet Sayarlıoğlu <ahmet.sayarlioglu@gmail.com>
2023-01-23 23:02:59 +0100

    added mailto

--- modules/Contact/Contact.tsx
+++ modules/Contact/Contact.tsx
@@ -10,7 +10,7 @@ export const Contact = () => {
       <h4>Telefon</h4>
       <div>+905424862454</div>
       <h4>Email</h4>
-      <div>info@pickiesapp.com</div>
+      <a href="mailto:info@pickiesapp.com">info@pickiesapp.com</a>
       <h4>Adres</h4>
       <div>SEYRANTEPE MAH. GÖKHAN SK. SERİNGÜL SİTESİ K2 BLOK NO: 18 İÇ KAPI NO: 2 KAĞITHANE İstanbul Turkey</div>
     </div>
