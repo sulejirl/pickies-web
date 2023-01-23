c4f50b0a373b54b8e2ac885848e77767d9459c51
Ahmet Sayarlıoğlu <ahmet.sayarlioglu@gmail.com>
2023-01-23 22:53:58 +0100

    contact page added

---
+++ modules/Contact/Contact.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "./contact.module.css"
+
+
+export const Contact = () => {
+  return (
+    <div className={style.container}>
+      <h1>İletişim</h1>
+      <div>Bize email ve whatsapp hattımızdan ulaşabilirsiniz</div>
+      <h4>Telefon</h4>
+      <div>+905424862454</div>
+      <h4>Email</h4>
+      <div>info@pickiesapp.com</div>
+      <h4>Adres</h4>
+      <div>SEYRANTEPE MAH. GÖKHAN SK. SERİNGÜL SİTESİ K2 BLOK NO: 18 İÇ KAPI NO: 2 KAĞITHANE İstanbul Turkey</div>
+    </div>
+  )
+}
---
+++ pages/contact/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Contact } from '@modules'
+
+
+const index = () => {
+    return (
+        <Contact />
+    )
+}
+
+export default index
